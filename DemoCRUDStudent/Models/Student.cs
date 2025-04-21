4e686c07844583abeac3f91c4ae8ed60bfd6357e
Administrator <Administrator@ISTAR_GISLAB_01>
2025-04-21 11:56:50 +0530

    Add project files.

---
+++ DemoCRUDStudent/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DemoCRUDStudent.Models;
+
+public partial class Student
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string? Gender { get; set; }
+
+    public int? Age { get; set; }
+}
---
+++ DemoCRUDStudent/Models/StudentdbContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoCRUDStudent.Models;
+
+public partial class StudentdbContext : DbContext
+{
+    public StudentdbContext()
+    {
+    }
+
+    public StudentdbContext(DbContextOptions<StudentdbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__student__3213E83F5C26D8DA");
+
+            entity.ToTable("student");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Age).HasColumnName("age");
+            entity.Property(e => e.Gender)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("gender");
+            entity.Property(e => e.Name)
+                .HasMaxLength(200)
+                .IsUnicode(false)
+                .HasColumnName("name");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
